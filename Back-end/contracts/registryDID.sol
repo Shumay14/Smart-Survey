21eb4e3762a1c639c9907edc8b2e56c0cba6dbd2
Shumay14 <seo533481@naver.com>
2021-11-03 23:55:00 +0900

    Update registryDID.sol

--- Back-end/contracts/registryDID.sol
+++ Back-end/contracts/registryDID.sol
@@ -1,11 +1,18 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+import "../models/createDID.js";
+
 contract registryDID {
 
   address owner;
   uint owner_id;
 
-  address[] public dids;
+  repository[] public dids;
+
+  modifier isOwner(uint _owner) {
+        require(dids[_owner].owner == msg.sender);
+        _;
+    }
 
   mapping(address => getDID) public registeredDid;
 
@@ -21,6 +28,16 @@ contract registryDID {
         dids.push(_address);
         owner_id = dids.length;
   }
+
+  
+
+   function createCredential( string memory _name, string memory _licenseNum, string memory _localAddress, string memory _periodStart, string memory periodEnd, string memory LicenseClass, string memory issueDate) public contractIsDIDOwner(_DIDRepositoryAddress, DID_Id) returns(bool){
+        credentialOwner.push(driveLicense( _name, _licenseNum, _localAddress, _periodStart, periodEnd, LicenseClass, issueDate));
+
+        emit DIDCreated(msg.sender, didId);
+        return true;
+    }
+  
 
 
 
