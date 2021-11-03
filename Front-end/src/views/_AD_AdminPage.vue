322d85254927dbe8f22fb9e9a74662fabba78f52
wkjung0624 <>
2021-11-03 16:07:00 +0900

    설문 등록 페이지 생성

---
+++ Front-end/src/views/_AD_AdminPage.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="main-container">
+
+        <Header />
+
+        <Breadcrumb :items="items" title="Blog Details" />
+
+        <AdminWrapper />
+
+        <Footer />
+
+        <OffCanvasMobileMenu />
+
+        <!-- back to top start -->
+        <back-to-top class="scroll-top" bottom="60px">
+            <i class="ion-android-arrow-up"></i>
+        </back-to-top>
+        <!-- back to top end -->
+    </div>
+</template>
+
+<script>
+import Header from '@/components/_Header';
+    import Breadcrumb from '../components/Breadcrumb'
+    import AdminWrapper from '../components/sections/_AD_AdminWrapper'
+    import Footer from '../components/Footer'
+    import OffCanvasMobileMenu from '@/components/OffCanvasMobileMenu'
+
+    export default {
+        components: {
+            Header,
+            Breadcrumb,
+            AdminWrapper,
+            Footer,
+            OffCanvasMobileMenu
+        },
+        data() {
+            return {
+                items: [
+                    {
+                        text: 'Home',
+                        to: "/"
+                    },
+                    {
+                        text: 'Blog Details',
+                        active: true
+                    }
+                ]
+            }
+        },
+        metaInfo: {
+            title: 'Castro - Blog Details',
+            htmlAttrs: {
+                lang: 'en',
+                amp: true
+            }
+        }
+    }
+</script>
+
