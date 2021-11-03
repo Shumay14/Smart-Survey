2aa3d6d58b82d9aeb073fd03b684b31455d37635
Shumay14 <seo533481@naver.com>
2021-11-03 16:47:13 +0900

    Update createDID.js

--- Back-end/models/createDID.js
+++ Back-end/models/createDID.js
@@ -1,3 +1,13 @@
+const DIDWallet = require('@transmute/did-wallet');
+const ES256K = require('@transmute/es256k-jws-ts');
+const jsonpatch = require('fast-json-patch');
+
+const { encodeJson } = require('../../func');
+
+const elementCrypto = require('../../crypto');
+const MnemonicKeySystem = require('../../crypto/MnemonicKeySystem');
+
+
 // Generate a simple did document model
 const didDocumentModel = getDidDocumentModel(
   primaryKey.publicKey,
@@ -30,3 +40,61 @@ const getDidDocumentModel = (primaryPublicKey, recoveryPublicKey) => ({
     },
   ],
 });
+
+const walletToInitialDIDDoc = wallet => {
+  const didDocumentModel = {
+    '@context': [
+      'https://www.w3.org/ns/did/v1',
+      'https://docs.element-did.com/contexts/sidetree/sidetree-v0.1.jsonld',
+    ],
+  };
+
+  const publicKeys = [];
+  const commonVerificationMethods = [];
+  const keyAgreementkeys = [];
+
+  Object.values(wallet.keys).forEach(walletKey => {
+    if (walletKey.type === 'assymetric') {
+      if (walletKey.tags[0] === 'X25519KeyAgreementKey2019') {
+        keyAgreementkeys.push({
+          id: walletKey.tags[1] || `#${walletKey.kid}`,
+          type: 'X25519KeyAgreementKey2019',
+          usage: 'signing',
+          [walletKey.didPublicKeyEncoding]: walletKey.publicKey,
+        });
+      } else {
+        if (walletKey.encoding === 'jwk') {
+          publicKeys.push({
+            id: walletKey.tags[1] || `#${walletKey.kid}`,
+            type: walletKey.tags[0],
+            usage: 'signing',
+            [walletKey.didPublicKeyEncoding]: JSON.parse(walletKey.publicKey),
+          });
+        } else {
+          publicKeys.push({
+            id:
+              walletKey.tags[1] !== undefined
+                ? walletKey.tags[1]
+                : `#${walletKey.kid}`,
+            type: walletKey.tags[0],
+            usage:
+              walletKey.tags[1] &&
+              walletKey.tags[1].split('#').pop() === 'recovery'
+                ? 'recovery'
+                : 'signing',
+            [walletKey.didPublicKeyEncoding]: walletKey.publicKey,
+          });
+        }
+
+        commonVerificationMethods.push(publicKeys[publicKeys.length - 1].id);
+      }
+    }
+  });
+  didDocumentModel.publicKey = publicKeys;
+  didDocumentModel.authentication = commonVerificationMethods;
+  didDocumentModel.assertionMethod = commonVerificationMethods;
+  didDocumentModel.capabilityDelegation = commonVerificationMethods;
+  didDocumentModel.capabilityInvocation = commonVerificationMethods;
+  didDocumentModel.keyAgreement = keyAgreementkeys;
+  // return didDocumentModel;
+}
