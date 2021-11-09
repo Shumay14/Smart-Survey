469d59f8e77c0a48be4ebb212e95876d6c8b35d4
Doku9 <fixbox7725@gmail.com>
2021-11-09 10:37:00 +0900

    upload web-extension

---
+++ Web-extension/README.md
@@ -0,0 +1,2 @@
+# DID-Chrome-Extension
+VC에서 VP를 추출할 수 있게 하는 확장프로그램입니다.
---
+++ Web-extension/vp-Maker/src/popup/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div style="width:400px">
+    <input type="number" v-bind="a">
+    <input type="number" v-bind="b">
+    <button @click="add()">
+      ADD A + B
+    </button>
+    <h1>add value {{ c }}</h1>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        a: 0,
+        b: 0,
+        c: 100
+      }
+    },
+    methods: {
+      add() {
+        this.c = 500
+        console.log("a")
+
+      }
+    }
+  }
+</script>
+
+<style lang="sass"></style>
