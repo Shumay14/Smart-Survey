606a82d3aadb0cbc6d452376889b828552a722c3
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-11-15 07:18:40 +0900

    main 수정 중

--- Front-end/src/views/_Main.vue
+++ Front-end/src/views/_Main.vue
@@ -5,10 +5,9 @@
 
         <div class="container">
 
-
             <section class="row content">
                 <div class="d-flex justify-content-center">
-                    <img src="assets/img/logo-custom/Logo-full-03_example.png" class="title-img" alt="Brand Logo">
+                    <!-- <img src="assets/img/logo-custom/Logo-full-03_example.png" class="title-img" alt="Brand Logo"> -->
                 </div>
                 <p class="title-sub">
                     탈중앙화 신원인증 기반의 패널인증 설문 플랫폼
@@ -16,67 +15,98 @@
 
                 <!-- 서비스 정의 한마디 -->
                 <p class="title-main">
-                    국내 최초 블록체인 기반 설문 <strong>서베이존</strong>에서<br>
-                    설문에 참여하고 보상을 받아가세요.
+                    국내 최초 블록체인 기반 설문 <strong>서베이존</strong>,<br>
+                    설문조사에 참여해보세요.
                 </p>
                 
+
                 <div class="d-flex justify-content-center">
-                    <button class="btn-start"> 
+                    <button class="btn-start">
                         설문 조사 참여하기
                     </button>
                 </div>
 
+                <!-- 메인이미지 시작 -->
                 <div class="d-flex justify-content-center">
                     <!-- <img src="assets/img/main/main-2.png" class="img-main" v-bind:class="{ 'img-main--hidden' : !showNavbar }"/> -->
-                    <img src="assets/img/main/main-1.png" style="width:90%"/>
-                </div>
-            </section>
-        
-
-            <section class="row content">
-                <div class="col-6">
-                    <!-- 우리 서비스 소개 , 장점 -->
-                    <p class="title-sub2">
-                        탈중앙화 된 신원인증을 통한 <br>
-                        개인정보의 자기주권 보장
-                    </p>
-
-                    <p class="description">
-                        사용자의 모든 개인정보는 그 누구도 동의없이 확인할 수 없도록 암호화되어
-                        블록체인 네트워크 상에서 관리됩니다.
-                    </p>
-                </div>
-                <div class="col-6">
-                    <img src="assets/img/main/main-2.png">
-                     <!-- class="img-main" v-bind:class="{ 'img-main--hidden' : !showNavbar }"/> -->
-                </div>
-            </section>
-            
-            
-            <section class="row content">
-                <div class="col-6">
-                    <!-- 수익 창출 방법 -->
-                    <!-- <h2>설문 참여 시 내 정보 공개 범위를 설정하여 더 많은 보상을 획득하세요.</h2> -->
-                    
-                    <p class="title-sub2">
-                        설문 참여 시 정보 공개 범위 설정으로 추가 보상 획득
-                    </p>
-                    <p class="description">
-                        설문 의뢰업체가 요구하는 선택적 정보사항을 추가로 제공하고 더 많은 보상을
-                        획득 할 수 있습니다.
-                    </p>
-                </div>
-                <div class="col-6">
-                    <img src="assets/img/main/main-3.png" v-bind:class="{ 'img-main' : showNavbar }"/>
+                    <img src="assets/img/main/main1.png" class="mt-5" style="width:70%" />
                 </div>
             </section>
 
-            <section class="row justify-content-center content">
-                <div class="col-6">
-                    <!-- 우리 서비스 미래 가치 -->
-                    <!-- <h2>"탈중앙화 신원증명, 이미 많은 유저가 경험하고 있습니다."</h2> -->
+
+                <div>
+                    <!-- 스위치 버튼 시작 -->
+                    <button @click="mainState=true">패널</button>
+                    <button @click="mainState=false">의뢰업체</button>
+                    <!-- 스위치 버튼 끝 -->
                 </div>
-            </section>
+
+<!-- 패널 측 화면 시작 -->
+                <transition name="fade">
+                <section v-if="mainState==true">
+                    <!-- 섹션1 시작 -->
+                    <section class="row content">
+                        <div class="col-6">
+                            <p class="title-sub2">
+                                설문조사에 참여하고 <br>
+                                쏟아지는 <strong>보상</strong>을 챙겨보세요.
+                            </p>
+                            <p class="description">
+                                설문 의뢰업체가 요구하는 선택적 정보사항을 추가로 제공하고 더 많은 보상을
+                                획득 할 수 있습니다.
+                            </p>
+                        </div>
+                        <div class="col-1">
+
+                        </div>
+                        <div class="col-5">
+                            <img src="assets/img/main/main2.png" style="width:100%" />
+                        </div>
+                    </section>
+                    <!-- 섹션1 종료 -->
+
+                    <!-- 섹션2 시작 -->
+                    <section class="row content">
+
+                        <div class="col-5">
+                            <img src="assets/img/main/main3.png" style="width:100%" />
+                            <!-- class="img-main" v-bind:class="{ 'img-main--hidden' : !showNavbar }"/> -->
+                        </div>
+<div class="col-1">
+
+                        </div>
+                        <div class="col-6">
+                            <h1 class="title-sub2">
+                                DID활용을 통한 <br>
+                                철저한 <strong>개인정보 보호</strong>
+                            </h1>
+
+                            <p class="description">
+                                사용자의 모든 개인정보는 그 누구도 동의없이 확인할 수 없도록 암호화되어
+                                블록체인 네트워크 상에서 관리됩니다.
+                            </p>
+                        </div>
+                    </section>
+                    <!-- 섹션2 종료 -->
+                </section>
+                </transition>
+                
+<!-- 패널 측 화면 종료 -->
+
+
+                <section v-if="mainState==false">
+                    <div>
+                        하하하
+                    </div>
+                </section>
+
+
+                <!-- <section class="row justify-content-center content">
+                    <div class="col-6">
+                        <h2>"탈중앙화 신원증명, 이미 많은 유저가 경험하고 있습니다."</h2>
+                    </div>
+                </section> -->
+
         </div>
 
         <Footer />
@@ -111,7 +141,6 @@
 </template>
 
 <script>
-
     import Header from '@/components/_Header';
     import Footer from '../components/Footer';
     import OffCanvasMobileMenu from '@/components/_OffCanvasMobileMenu';
@@ -124,13 +153,13 @@
         },
         data() {
             return {
-                showNavbar_a : true,
-                showNavbar_b : true,
-                
-                lastScrollPosition : 0,
-                
-                items: [
-                    {
+                showNavbar_a: true,
+                showNavbar_b: true,
+                mainState: true,
+
+                lastScrollPosition: 0,
+
+                items: [{
                         text: 'Home',
                         to: "/"
                     },
@@ -142,102 +171,108 @@
             }
         },
         mounted() {
-            window.addEventListener('scroll', this.onScroll) 
+            window.addEventListener('scroll', this.onScroll)
         },
-        beforeDestroy() { 
-            window.removeEventListener('scroll', this.onScroll) 
+        beforeDestroy() {
+            window.removeEventListener('scroll', this.onScroll)
         },
         methods: {
-            onScroll () {
+            onScroll() {
                 // Get the current scroll position
                 const currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop
                 // console.log(currentScrollPosition)
                 // Because of momentum scrolling on mobiles, we shouldn't continue if it is less than zero 
-                if (currentScrollPosition < 0) { return }
+                if (currentScrollPosition < 0) {
+                    return
+                }
                 // Here we determine whether we need to show or hide the navbar 
-                this.showNavbar = currentScrollPosition < this.lastScrollPosition 
+                this.showNavbar = currentScrollPosition < this.lastScrollPosition
                 // Set the current scroll position as the last scroll position 
-                this.lastScrollPosition = currentScrollPosition 
+                this.lastScrollPosition = currentScrollPosition
             },
         }
     }
 </script>
 
 <style lang="scss" scoped>
-
-    strong{
-        color:RGB(238,111,0);
+    strong {
+        color: RGB(238, 111, 0);
     }
 
-    .container{
+    .container {
         padding-top: 80px;
     }
 
-    .content{
+    .content {
         align-items: center;
         padding-bottom: 180px;
         word-break: keep-all;
     }
 
-    .btn{
-        &-start{
-            background-color: RGB(238,111,0);
-            
+    .btn {
+        &-start {
+            background-color: RGB(238, 111, 0);
+
             border-radius: 10px;
             border: 0px;
 
-            color:white;
+            color: white;
             font-weight: bold;
             font-size: 20px;
-            
-            width:300px;
-            height:60px;
+
+            width: 300px;
+            height: 60px;
         }
     }
 
-    .title{
-        &-main{
-            padding-top:60px;
+    .title {
+        &-main {
+            // padding-top: 20px;
             font-size: 55px;
             text-align: center;
             font-weight: bold;
             letter-spacing: -2.72px;
         }
-        &-sub{
-            padding-top:20px;
+
+        &-sub {
+            padding-top: 20px;
             font-size: 24px;
             text-align: center;
             letter-spacing: -0.72px;
         }
-        &-sub2{
+
+        &-sub2 {
             font-size: 40px;
             font-weight: bold;
             line-height: 1.55;
             letter-spacing: -0.12rem;
         }
-        &-img{
+
+        &-img {
             width: 150px;
             height: 33px;
             object-fit: cover;
         }
     }
-    .img-main{
-       height: 60px; 
-       width: 100vw; 
-       background: hsl(200, 50%, 50%); 
-       position: fixed; 
-       box-shadow: 0 2px 15px rgba(71, 120, 120, 0.5); 
-       transform: translate3d(0, 0, 0); 
-       transition: 0.1s all ease-out;
+
+    .img-main {
+        height: 60px;
+        width: 100vw;
+        background: hsl(200, 50%, 50%);
+        position: fixed;
+        box-shadow: 0 2px 15px rgba(71, 120, 120, 0.5);
+        transform: translate3d(0, 0, 0);
+        transition: 0.1s all ease-out;
 
         //fade in 효과 추가필요
 
-        &--hidden{
+        &--hidden {
             box-shadow: none;
             transform: translate3d(0, -100%, 0);
         }
     }
-    .description{
+
+    .description {
         padding-top: 30px;
         font-size: 20px;
         font-weight: 500;
@@ -247,4 +282,11 @@
 
         word-break: keep-all;
     }
+
+    .fade-enter-active, .fade-leave-active {
+  transition: opacity .5s;
+}
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
