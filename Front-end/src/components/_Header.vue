1e3b7d716d730ef715389de09b1e879049e0442f
Doku9 <86043933+Doku9@users.noreply.github.com>
2021-11-18 07:21:20 +0900

    header router-link

--- Front-end/src/components/_Header.vue
+++ Front-end/src/components/_Header.vue
@@ -1,43 +1,43 @@
 <template>
   <!--====================  header area ====================-->
   <fixed-header>
-    <div class="header-area" >
-      <fixed-header >
-      <div class="header-area__desktop header-area__desktop--default mainHeader">
-        <!--=======  header top bar  =======-->
+    <div class="header-area">
+      <fixed-header>
+        <div class="header-area__desktop header-area__desktop--default mainHeader">
+          <!--=======  header top bar  =======-->
 
-        <!--=======  End of header top bar   =======-->
-        <div class="header-info-area">
-          <div class="container">
-            <div class="row align-items-center">
-              <div class="col-lg-12">
-                <div class="header-info-wrapper align-items-center">
-                  <!-- logo START -->
-                  <div class="logo">
-                    <router-link to="/">
-                      <img src="assets/img/logo-custom/Logo-text.png" width="150%" class="img-fluid" style=""
-                        alt="Brand Logo">
-                    </router-link>
-                  </div>
-                  <!-- logo END -->
+          <!--=======  End of header top bar   =======-->
+          <div class="header-info-area">
+            <div class="container">
+              <div class="row align-items-center">
+                <div class="col-lg-12">
+                  <div class="header-info-wrapper align-items-center">
+                    <!-- logo START -->
+                    <div class="logo">
+                      <router-link to="/">
+                        <img src="assets/img/logo-custom/Logo-text.png" width="150%" class="img-fluid" style=""
+                          alt="Brand Logo">
+                      </router-link>
+                    </div>
+                    <!-- logo END -->
 
-                  <!-- header contact info start -->
-                  <div class="header-contact-info">
-                    <!-- 네비게이션 -->
-                    <div class="
+                    <!-- header contact info start -->
+                    <div class="header-contact-info">
+                      <!-- 네비게이션 -->
+                      <div class="
                         header-navigation header-navigation--header-default
                         position-relative
                       ">
-                      <div class="header-navigation__nav position-static my-auto">
-                        <nav>
-                          <ul>
+                        <div class="header-navigation__nav position-static my-auto">
+                          <nav>
+                            <ul>
 
-                            <!-- <li class="
+                              <!-- <li class="
                                 has-children has-children--multilevel-submenu
                               "> -->
-                            <li>
-                              <router-link to="/surveylist">설문조사</router-link>
-                              <!-- <ul class="submenu">
+                              <li>
+                                <router-link to="/surveylist">설문조사</router-link>
+                                <!-- <ul class="submenu">
                                 <li>
                                   <router-link to="/surveylist">전체 설문조사</router-link>
                                 </li>
@@ -46,129 +46,139 @@
                                   </router-link>
                                 </li>
                               </ul> -->
-                            </li>
+                              </li>
 
-                            <li>
-                              <router-link to="/admin">설문 등록</router-link>
-                            </li>
+                              <li>
+                                <router-link to="/admin">설문 등록</router-link>
+                              </li>
 
-                            <li class="
+                              <li class="
                                 has-children has-children--multilevel-submenu
                               ">
-                              <router-link to="/notice">고객센터</router-link>
-                              <ul class="submenu">
-                                <li>
-                                  <router-link to="/notice">공지사항</router-link>
-                                </li>
-                                <li>
-                                  <router-link to="/oneonone">1:1 문의</router-link>
-                                </li>
-                                <li>
-                                  <router-link to="/faq">FAQ</router-link>
-                                </li>
-                              </ul>
-                            </li>
+                                <router-link to="/notice">고객센터</router-link>
+                                <ul class="submenu">
+                                  <li>
+                                    <router-link to="/notice">공지사항</router-link>
+                                  </li>
+                                  <li>
+                                    <router-link to="/oneonone">1:1 문의</router-link>
+                                  </li>
+                                  <li>
+                                    <router-link to="/faq">FAQ</router-link>
+                                  </li>
+                                </ul>
+                              </li>
 
-                            <li class="
+                              <li class="
                                 has-children has-children--multilevel-submenu
                               ">
-                              <router-link to="/mypage">마이페이지</router-link>
-                              <ul class="submenu">
-                                <li>
-                                  <router-link to="/mypage">마이페이지</router-link>
-                                </li>
-                                <li>
-                                  <router-link to="/favorite">
-                                    관심사 설정
-                                  </router-link>
-                                </li>
-                              </ul>
-                            </li>
+                                <router-link to="/mypage">마이페이지</router-link>
+                                <ul class="submenu">
+                                  <li>
+                                    <router-link to="/mypage">마이페이지</router-link>
+                                  </li>
+                                  <li>
+                                    <router-link to="/favorite">
+                                      관심사 설정
+                                    </router-link>
+                                  </li>
+                                </ul>
+                              </li>
 
-                          </ul>
-                        </nav>
+                            </ul>
+                          </nav>
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div class="col-3 metamask_div desktop-menu-icon my-auto">
-                    <!-- 메타마스크 아이콘 시작-->
-                    <label class="metamask_Logo">
-                      <MetamaskLogo v-if="this.$store.state.metamaskAdd == null" />
-                      <img :src="user_avatar" style="width: 2em" alt="" v-if="this.$store.state.metamaskAdd != null" />
-                    </label>
-                    <!-- 메타마스크 아이콘 종료 -->
+                    <div class="col-3 metamask_div desktop-menu-icon my-auto">
+                      <!-- 메타마스크 아이콘 시작-->
+                      <label class="metamask_Logo">
+                        <MetamaskLogo v-if="this.$store.state.metamaskAdd == null" />
+                        <img :src="user_avatar" style="width: 2em" alt=""
+                          v-if="this.$store.state.metamaskAdd != null" />
+                      </label>
+                      <!-- 메타마스크 아이콘 종료 -->
 
-                    <!-- 로그인 버튼 시작-->
-                    <label type="button" class="metamask_Logo_Button btn btn-dark"
-                      v-if="this.$store.state.metamaskAdd == null" style="width: 7em">
-                      로그인
-                      <Web3 />
-                    </label>
-                    <!-- 로그인 버튼 종료 -->
+                      <!-- 로그인 버튼 시작-->
+                      <label type="button" class="metamask_Logo_Button btn btn-dark"
+                        v-if="this.$store.state.metamaskAdd == null" style="width: 7em">
+                        로그인
+                        <Web3 />
+                      </label>
+                      <!-- 로그인 버튼 종료 -->
 
-                    <!-- 로그아웃버튼 시작 -->
-                    <label type="button" class="btn btn-dark metamask_Logo_Button" style="width: 8em" @click="logout()"
-                      v-show="this.$store.state.metamaskAdd != null">{{ this.$store.state.metamaskShortAdd }}
-                    </label>
-                    <!-- 로그아웃버튼 종료 -->
-                  </div>
+                      <!-- 로그아웃버튼 시작 -->
+                      <label type="button" class="btn btn-dark metamask_Logo_Button" style="width: 8em"
+                        @click="logout()"
+                        v-show="this.$store.state.metamaskAdd != null">{{ this.$store.state.metamaskShortAdd }}
+                      </label>
+                      <!-- 로그아웃버튼 종료 -->
+                    </div>
 
-                  <!-- mobile menu -->
-                  <div class="mobile-navigation-icon" id="mobile-menu-trigger"
-                    @click="mobiletoggleClass('addClass', 'active')">
-                    <i></i>
+                    <!-- mobile menu -->
+                    <div class="mobile-navigation-icon" id="mobile-menu-trigger"
+                      @click="mobiletoggleClass('addClass', 'active')">
+                      <i></i>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-      </div>
+        </div>
       </fixed-header>
 
       <!-- <fixed-header > -->
-        <!--=======  End of header info area =======-->
-        <div class="header-top-bar subHeader">
-          <div class="container">
-            <div class="top-info-bar row align-items-center" >
+      <!--=======  End of header info area =======-->
+      <div class="header-top-bar subHeader">
+        <div class="container">
+          <div class="top-info-bar row align-items-center">
 
-              <div class="info-menu--item col-3">
-                <div class="info-menu--item--head">총 설문 금액</div>
-                <div class="info-menu--item--data ">
-                  ￦ {{ getCurrentSurveyInfo("total") }}
-                </div>
-              </div>
+            <div class="info-menu--item col-3">
+                <router-link to="/surveylist">
+                  <div class="info-menu--item--head">총 설문 금액</div>
+                  <div class="info-menu--item--data ">
+                    ￦ {{ getCurrentSurveyInfo("total") }}
+                  </div>
+                </router-link>
+            </div>
 
-              <div class="info-menu--item col-3">
-                <div class="info-menu--item--head ">
-                  참여 가능한 설문
-                </div>
-                <div class="info-menu--item--data">
-                  {{ getCurrentSurveyInfo("count") }} 개
-                </div>
-              </div>
+            <div class="info-menu--item col-3">
+                <router-link to="/surveylist">
+                  <div class="info-menu--item--head ">
+                    참여 가능한 설문
+                  </div>
+                  <div class="info-menu--item--data">
+                    {{ getCurrentSurveyInfo("count") }} 개
+                  </div>
+                </router-link>
+            </div>
 
-              <div class="info-menu--item col-3">
-                <div class="info-menu--item--head">
-                  획득 가능한 수익
-                </div>
-                <div class="info-menu--item--data">
-                  ￦ {{ getCurrentSurveyInfo("reward") }}
-                </div>
-              </div>
+            <div class="info-menu--item col-3">
+                <router-link to="/surveylist">
+                  <div class="info-menu--item--head">
+                    획득 가능한 수익
+                  </div>
+                  <div class="info-menu--item--data">
+                    ￦ {{ getCurrentSurveyInfo("reward") }}
+                  </div>
+                </router-link>
+            </div>
 
-              <div class="info-menu--item col-3">
-                <div class="info-menu--item--head">총 획득한 수익</div>
-                <div class="info-menu--item--data">￦ 0</div>
-              </div>
-
+            <div class="info-menu--item col-3">
+                <router-link to="/mypage">
+                  <div class="info-menu--item--head">총 획득한 수익</div>
+                  <div class="info-menu--item--data">￦ 0</div>
+                </router-link>
             </div>
 
           </div>
+
         </div>
-        <!-- 헤더 추가 정보 끝 -->
+      </div>
+      <!-- 헤더 추가 정보 끝 -->
       <!-- </fixed-header> -->
 
 
@@ -274,10 +284,10 @@
     animation: 900ms cubic-bezier(0.2, 1, 0.22, 1) 0s normal none 1 running fadeInDown;
   }
 
-  .top-info-bar {
-    padding-top: 0.4rem;
-    padding-bottom: 0.4rem;
-  }
+  // .top-info-bar {
+  //   padding-top: 0.4rem;
+  //   padding-bottom: 0.4rem;
+  // }
 
   .info-menu {
     &--item {
@@ -337,5 +347,4 @@
       }
     }
   }
-  
 </style>
