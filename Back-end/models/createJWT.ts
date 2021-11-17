8b9e95e97d3f06959c89195a5845d1e8f7f6cdc2
wkjung0624 <>
2021-11-17 13:52:46 +0900

    VC, VP 구조 일부 수정

--- Back-end/models/createJWT.ts
+++ Back-end/models/createJWT.ts
@@ -1,14 +1,11 @@
 // convert data into canonical(standard or normal) format
 import canonicalizeData from 'canonicalize'
-
 import { encodeBase64url } from './util'
 
-
-const defaultAlg = 'ES256K'
-
+const defaultAlg = 'X25519_XSalsa20_Poly1305'
 
 // export type getVC = string
-export type publicKey = string
+// export type publicKey = string
 // export type rawData = string
 
 export interface JWTHeader {
@@ -16,48 +13,44 @@ export interface JWTHeader {
   alg: string
 }
   
-  
 // issuer, subject, audience, issued at, Not Before, expiration
-export interface JWTPayload {
-  title?: string              
-  sub?: string
-  data?: string //encrypt data
+export interface JWTPayload {   
+  sub?: string // 해당 VC의 주인
+  title?: string // VC 항목명
+  data?: string // encrypt data
 }
 
-
-
-
-
+export interface JWTSignature {   
+  rawData?: string // 개인정보의 원본
+  pubKey?: string // 원본을 암호화 한 설문 의뢰자의 공개키
+}
 
 // create jwt-did-vc
 // return aaaa.bbbb
-export async function createVC(
+export function createVC(
     header: Partial<JWTHeader>,
     payload: string | Partial<JWTPayload>,
 
-): Promise<string> {
+): string{
     if (!header.alg) header.alg = defaultAlg
-    const encodedPayload = typeof payload === 'string' ? payload : encodeSection(payload)
-    const endcodedHeader: string = [encodeSection(header), encodedPayload].join('.')
-  
+
+    // encodeSection == Base64 Encode Function, 사용자 정의 함수
+    const endcodedHeader: string = encodeSection(header)
+    const encodedPayload: string = encodeSection(payload) 
+    
     return [endcodedHeader, encodedPayload].join('.')
 }
 
 
 // create jwt-did-vp
 // return aaaa.rawData.pubKey
-export async function createVP(
+export function createVP(
     header: Partial<JWTHeader>,
     payload: string | Partial<JWTPayload>,
-    pubKey: publicKey,
-    rawData: string,
-
-): Promise<string> {
-  const VC = createVC(header, payload)
-  
-  return [VC, rawData, pubKey].join('.')
+    signature: string | Partial<JWTSignature>
+): string {
+  return [header, payload, signature].join('.')
 }
-
 
 
 function encodeSection(data: any, shouldCanonicalize = false): string {
