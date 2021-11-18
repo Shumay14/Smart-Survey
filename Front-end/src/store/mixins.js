a98433a14cfecfd6b755d68e78946bbe1dcf6ef5
wkjung0624 <>
2021-11-19 00:34:43 +0900

    api 함수 수정

--- Front-end/src/store/mixins.js
+++ Front-end/src/store/mixins.js
@@ -1,24 +1,25 @@
 import axios from "axios";
 
 export default {
-  methods: {
-    async $api(_method, _url, _data) {
-      return (
-        await axios({
-          method: _method, // get, post
-          url: _url, // 127.0.0.1:3000
-          data: _data,
-        }).catch((e) => {
-          console.log(e);
-        })
-      ).data;
+    methods: {
+        async $api(_method, _url, data) {
+            return (
+                await axios({
+                    method: _method, // get, post
+                    url: _url, // 127.0.0.1:3000
+                    data,
+                    // data: , 포스트에만 쓰임
+                }).catch((e) => {
+                    console.log(e);
+                })
+            ).data;
+        },
+        $base64(file) {
+            return new Promise((resolve) => {
+                var a = new FileReader();
+                a.onload = (e) => resolve(e.target.result);
+                a.readAsDataURL(file);
+            });
+        },
     },
-    $base64(file) {
-      return new Promise((resolve) => {
-        var a = new FileReader();
-        a.onload = (e) => resolve(e.target.result);
-        a.readAsDataURL(file);
-      });
-    },
-  },
 };
