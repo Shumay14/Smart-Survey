b961f0b5108e4488181d4a7392be290dc8deec6d
wkjung0624 <>
2021-11-19 02:00:20 +0900

    SQL 세미콜론 제거

--- Back-end/config/sql.js
+++ Back-end/config/sql.js
@@ -1,19 +1,10 @@
 module.exports = {
     getSurveyList: {
-        cmd: `SELECT * FROM T_SURVEY;`
+        cmd: `SELECT * FROM T_SURVEY`
     },
     addSurveyItem: {
         cmd: `INSERT INTO T_SURVEY VALUES
-            (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
+            (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)`
         //idx, category, img, title, desc, sdate, edate, reward, vp
     },
-
-    getUserInterest: {
-        cmd: `update t_user set artist=1
-                where email=? provider=?;`
-    },
-    setUserInterest: {
-        cmd: `update t_user set artist=1
-                where email=? provider=?;`
-    }
 }
