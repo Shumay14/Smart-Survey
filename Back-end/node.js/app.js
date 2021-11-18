cdbec616bbd4fd9c49187372c39200b666ddc113
Welsh Corgi <86044038+UiCheonKim@users.noreply.github.com>
2021-11-18 13:30:42 +0900

    slider

--- Back-end/node.js/app.js
+++ Back-end/node.js/app.js
@@ -1,94 +1,117 @@
-const express = require('express')
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 // const bodyParser = require('body-parser');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 const db = {
-    host: 'surveyzone.cqqcyxlpgd6h.ap-northeast-2.rds.amazonaws.com',
-    user: 'corgi',
-    password: 'corgi123!',
-    database: 'surveyzone',
-    connectionLimit: 10
+  host: "surveyzone.cqqcyxlpgd6h.ap-northeast-2.rds.amazonaws.com",
+  user: "corgi",
+  password: "corgi123!",
+  database: "surveyzone",
+  connectionLimit: 10,
 };
 
-const dbPool = require('mysql').createPool(db);
+const dbPool = require("mysql").createPool(db);
 
 // app.use(bodyParser.json());
 app.use(cors());
-app.use(express.json({
-    limit: '50mb'
-}));
-app.use(express.urlencoded({
-    extends: true
-}))
+app.use(
+  express.json({
+    limit: "50mb",
+  })
+);
+app.use(
+  express.urlencoded({
+    extends: true,
+  })
+);
 
 // GET Methods START
-app.get('/', (req, res) => {
-    res.send('API SERVER OPEN');
-})
+app.get("/", (req, res) => {
+  res.send("API SERVER OPEN");
+});
 
-app.get('/api/survey', (req, res) => {
-    var where = req.body;
-    dbPool.getConnection(function (err, conn) {
-        if (!err) {
-            conn.query('SELECT * FROM T_SURVEY', function (err, result, fields) {
-                if (err) throw err;
-                res.send(result);
-            });
-        }
-        conn.release();
-    });
-})
+app.get("/api/survey", (req, res) => {
+  var where = req.body;
+  dbPool.getConnection(function (err, conn) {
+    if (!err) {
+      conn.query("SELECT * FROM T_SURVEY", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
+    }
+    conn.release();
+  });
+});
+
+app.get("/api/survey/interest", (req, res) => {
+  var where = req.body;
+  dbPool.getConnection(function (err, conn) {
+    if (!err) {
+      conn.query("SELECT * FROM T_USER", function (err, result, fields) {
+        if (err) throw err;
+        res.send(result);
+      });
+    }
+    conn.release();
+  });
+});
 // GET Methods END
 
-
 // POST Method START
-app.post('/', (req, res) => {
-    //x-www-form-urlencoded
-    console.log(req.body);
-    res.send(req.body);
-})
+app.post("/", (req, res) => {
+  //x-www-form-urlencoded
+  console.log(req.body);
+  res.send(req.body);
+});
 
-app.post('/api/survey', (req, res) => {
-    dbPool.getConnection(function (err, conn) {
-        var param = {
-            item: Object.keys(req.body)[0]
-        }
+app.post("/api/survey", (req, res) => {
+  dbPool.getConnection(function (err, conn) {
+    var param = {
+      item: Object.keys(req.body)[0],
+    };
 
-        if (!err) {
-            conn.query('INSERT INTO T_SURVEY SET ?', param, function (err, result, fields) {
-                if (err) throw err;
-                res.send(result);
-            });
+    if (!err) {
+      conn.query(
+        "INSERT INTO T_SURVEY SET ?",
+        param,
+        function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
         }
-    })
-})
+      );
+    }
+  });
+});
 // 아직 미완
-app.post('/api/category', (req, res) => {
-    dbPool.getConnection(function (err, conn) {
-        var param = {
-            item: req.body
+app.post("/api/category", (req, res) => {
+  dbPool.getConnection(function (err, conn) {
+    var param = {
+      item: req.body,
+    };
+
+    console.log(param);
+
+    console.log(req.body);
+    console.log(param.item, typeof param.item);
+
+    if (!err) {
+      conn.query(
+        "INSERT INTO T_SURVEY SET ?",
+        param,
+        function (err, result, fields) {
+          if (err) throw err;
+          res.send(result);
         }
-
-        console.log(param);
-
-        console.log(req.body);
-        console.log(param.item, typeof (param.item));
-
-        if (!err) {
-            conn.query('INSERT INTO T_SURVEY SET ?', param, function (err, result, fields) {
-                if (err) throw err;
-                res.send(result);
-            });
-        }
-    })
-})
+      );
+    }
+  });
+});
 // POST Method END
-
 
 // Express Server Start
 app.listen(port, () => {
-    console.log(`Survey-Zone API SERVER listening on port ${port}`)
-})
+  console.log(`Survey-Zone API SERVER listening on port ${port}`);
+});
