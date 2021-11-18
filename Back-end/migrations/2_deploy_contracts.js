7968794c575875eee798aff32dff110349634ecb
Shumay14 <seo533481@naver.com>
2021-11-18 16:14:25 +0900

    Update 2_deploy_contracts.js

--- Back-end/migrations/2_deploy_contracts.js
+++ Back-end/migrations/2_deploy_contracts.js
@@ -1,7 +1,9 @@
 
 var registryDID = artifact.require("./registryDID.sol");
+var Survey = artifact.require("./Survey.sol");
 
 module.exports = function(deployer) {
   
   deployer.deploy(registryDID);
+  deployer.deploy(Survey);
 };
