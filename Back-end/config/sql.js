ffc84fac8b21798c222fc8c7b1ee2b7195672004
Shumay14 <seo533481@naver.com>
2021-11-23 16:23:02 +0900

    VC DB Backend editing

--- Back-end/config/sql.js
+++ Back-end/config/sql.js
@@ -13,4 +13,12 @@ module.exports = {
             (NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`,
     //idx, category, img, title, desc, sdate, edate, reward, vp, url
   },
+  addUserItem: {
+    cmd: `INSERT INTO T_USER (NULL, ?, ?, ?, NULL)`,
+    //idx, account, kakao, interests, vclist
+  },
+  registerVC: {
+    cmd: `INSERT INTO T_USER(vclist) VALUES(?, ?, ?, ?)`,
+    //name, gender, age, income
+  }
 };
